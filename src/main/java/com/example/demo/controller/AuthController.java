4811b9ae5b8d9729a547706afa2ba6754f20c1c6
estevan.costa <estevan.costa@ifood.com.br>
2024-11-13 19:55:42 -0300

    Adição de verificação de usuário no banco

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,13 +12,16 @@ import com.example.demo.service.AuthService;
 public class AuthController {
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private UsuarioController controle;
     @PostMapping
     public ResponseEntity<?> login(@RequestBody UserCredentials userCredentials) {
-        // Aqui você deve validar o usuário (ex: verificar no banco de dados)
-        if ("user".equals(userCredentials.getUsername()) && "password".equals(userCredentials.getPassword())) {
-            Long userId = 1L;
-            String userType = "1";
-            String token = authService.generateToken(userCredentials.getUsername(), userId, userType);
+        Usuario encontrado = controle.verificaLogin(userCredentials.getUsername(), userCredentials.getPassword());
+        if (encontrado != null) {
+            Long userId = encontrado.getId();
+            String userType = encontrado.getNome();
+            String token = authService.generateToken(encontrado.getEmail(), userId, userType);
             return ResponseEntity.ok(new AuthResponse(token));
         } else {
             return ResponseEntity.status(401).body("Credenciais inválidas");
